b2d45ce27dca025dec15b5121ba4bf20b214fcf2
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-10 14:40:08 +0330

    installing HtmlWebpack(step 2)

--- HowConfig/src/webpack/webpack.config.js
+++ HowConfig/src/webpack/webpack.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.join(__dirname, "..", "./src/index.js"),
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
   output: {
     path: path.resolve(__dirname, "..", "./build"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "..", "./src/index.html"),
+    }),
+  ],
 };
