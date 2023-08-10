e83cea73828d5e24a20cd9a190ca74851dc115db
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-10 15:04:16 +0330

    re-order babelrc and webpack's folder

--- HowConfig/webpack/webpack.config.js
+++ HowConfig/webpack/webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "..", "./index.js"),
+  entry: path.resolve(__dirname, "..", "./src/index.js"),
   output: {
     path: path.resolve(__dirname, "..", "./build"),
   },
@@ -14,9 +14,7 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
-          },
+         
         },
       },
       {
@@ -27,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "..", "./index.html"),
+      template: path.join(__dirname, "..", "./src/index.html"),
     }),
   ],
 };
