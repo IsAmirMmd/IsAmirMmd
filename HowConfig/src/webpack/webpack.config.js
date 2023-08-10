f9b68bf7e7819c99129691db203f51c64d56faf2
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-08-10 14:42:34 +0330

    installing babel (step 3)

--- HowConfig/src/webpack/webpack.config.js
+++ HowConfig/src/webpack/webpack.config.js
@@ -6,6 +6,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "..", "./build"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "..", "./src/index.html"),
